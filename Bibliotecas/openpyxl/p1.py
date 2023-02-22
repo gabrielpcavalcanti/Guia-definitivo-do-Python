10e2ac92590ceddc1b84114d3612fe570650567c
Gabriel-Cavalcanti <programacaogabrielcavalcanti18@gmail.com>
2023-02-22 23:21:41 +0000

    Aulas openpyxl

--- Bibliotecas/openpyxl/p1.py
+++ Bibliotecas/openpyxl/p1.py
@@ -3,29 +3,3 @@ from openpyxl import Workbook, load_workbook
 wb = load_workbook('grades.xlsx')
 ws = wb.active
 print(ws)
-
-# give the value of a cell
-
-print(ws['A3'].value)
-
-# Change some value
-
-ws["A1"] = 'test'
-
-# Save the work
-
-wb.save('grades.xlsx')
-
-# Show all of the sheets in the excel file
-
-print(wb.sheetnames)
-
-# work with an expecifc sheet
-
-# ws = wb['sheet 1'] for example
-
-# Creating a new sheet 
-
-wb.create_sheet("Test")
-
-print(wb.sheetnames)
--- Bibliotecas/openpyxl/p5.py
+++
@@ -1,5 +0,0 @@
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
-
-wb = load_workbook("gabriel.xlsx")
-ws = wb.active
