8a50402586ad96c390904da0fc327b00ac342d47
gabrielpcavalcanti <57358184+gabrielpcavalcanti@users.noreply.github.com>
2025-02-24 20:34:14 -0300

    update

--- teste.py
+++ teste.py
@@ -1,15 +1,10 @@
-num_1 = 10
-num_2 = 15
-nome = 'Gabriel'
+l1 = [2,4,3]
+l2 = [5,6,4]
 
-# Format()
+l1.reverse()
+l2.reverse()
 
-string_01 = "Eu tenho {} reais e {} dolares e meu nome é {}.".format(num_1, num_2, nome)
+numeros = map(int, l1.split())
+variavel_numeros = numeros
 
-# +
-
-string_02 = "Eu tenho " + str(num_1) + " reais " "e " + str(num_2) +  " dolares e meu nome é " + nome+ "."
-
-print(string_02)
-
-print(f"Eu tenho {num_1} reais e {num_2} dolares e meu nome é {nome}.")
+print(variavel_numeros)
