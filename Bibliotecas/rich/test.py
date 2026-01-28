a41e7911b2fd22f05610fdaf9cefc3b6ec8fa1bb
gabrielpcavalcanti <57358184+gabrielpcavalcanti@users.noreply.github.com>
2026-01-27 22:52:29 -0300

    update 27/01/26

--- Bibliotecas/rich/test.py
+++ Bibliotecas/rich/test.py
@@ -2,6 +2,3 @@ from rich.console import Console
 
 console = Console()
 
-console.print("Gabriel Purificação Cavalcanti", style="bold")
-console.print("O processo foi feito com sucesso", style="italic green")
-console.print("[underline red]O processo foi feito com erro[/]")
