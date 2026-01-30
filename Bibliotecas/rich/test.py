254e42f87b979f563ea92b4e375d94ece9461670
gabrielpcavalcanti <57358184+gabrielpcavalcanti@users.noreply.github.com>
2026-01-30 15:05:04 -0300

    update 30/01/26

--- Bibliotecas/rich/test.py
+++ Bibliotecas/rich/test.py
@@ -1,11 +1,46 @@
-from random import randint
-from rich import print
-from rich.highlighter import Highlighter
+from rich.console import Console
 
-class RainbowHighlighter(Highlighter):
-    def highlight(self, text):
-        for index in range(len(text)):
-            text.stylize(f"color({randint(16, 255)})", index, index + 1)
-            
-rainbow = RainbowHighlighter()
-print(rainbow("I must not fear. Fear is the mind-killer."))
+console = Console()
+
+def sum(num1: float, num2: float) -> float:
+    return num1 + num2
+
+
+def subtraction(num1: float, num2: float) -> float:
+    return num1 - num2
+
+
+def multiplication(num1: float, num2: float) -> float:
+    return num1 * num2
+
+
+def division(num1: float, num2: float) -> float:
+    return num1 / num2
+
+
+def main() -> None:
+    res: str = console.input("Qual operação vc quer [yellow](soma, sub, mult ou div): [/]")
+    num_01: float = float(console.input("Digite o [bold green]primeiro número:  [/]"))
+    num_02: float = float(console.input("Digite o [bold green]segundo número:  [/]"))
+
+
+    match res:
+
+        case "soma":
+            console.print(f'A soma de {num_01} e {num_02} é [white]{sum(num_01, num_02)}[/]' )
+        
+        case "sub":
+            console.print(subtraction(num_01, num_02))
+        
+        case "mult":
+            console.print(multiplication(num_01, num_02))
+        
+        case "div":
+            console.print(division(num_01, num_02))
+        
+        case _:
+            print("Valor inválido")
+ 
+ 
+if __name__ == '__main__':
+    main()
