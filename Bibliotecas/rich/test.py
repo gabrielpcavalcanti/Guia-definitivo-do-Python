d5c81895d9d600217c6a7f27545d63e3c2642029
gabrielpcavalcanti <57358184+gabrielpcavalcanti@users.noreply.github.com>
2026-01-30 00:01:16 -0300

    update 29/01/26

--- Bibliotecas/rich/test.py
+++ Bibliotecas/rich/test.py
@@ -1,4 +1,11 @@
-from rich.console import Console
+from random import randint
+from rich import print
+from rich.highlighter import Highlighter
 
-console = Console()
-
+class RainbowHighlighter(Highlighter):
+    def highlight(self, text):
+        for index in range(len(text)):
+            text.stylize(f"color({randint(16, 255)})", index, index + 1)
+            
+rainbow = RainbowHighlighter()
+print(rainbow("I must not fear. Fear is the mind-killer."))
